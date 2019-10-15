741548b31a51099e2c361e888c75df5128391672
TangRufus <tangrufus@gmail.com>
2019-10-15 15:47:44 +0100

    Update code standards

--- gf-sagepay.php
+++ gf-sagepay.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name:     GF SagePay
  * Plugin URI:      https://www.itineris.co.uk/
@@ -22,15 +23,4 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
-/**
- * Begins execution of the plugin.
- *
- * @return void
- */
-function run(): void
-{
-    $plugin = new GFSagePay();
-    $plugin->run();
-}
-
-run();
+GFSagePay::run();
--- src/GFSagePay.php
+++ src/GFSagePay.php
@@ -11,7 +11,7 @@ class GFSagePay
 {
     public const VERSION = '0.9.6';
 
-    public function run(): void
+    public static function run(): void
     {
         // TODO: Check `\GFForms` is loaded.
         GFForms::include_payment_addon_framework();
